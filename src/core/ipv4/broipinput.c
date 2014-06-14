99bf36934abce3e042314f77a962cfbeca7ee98e
sla89 <skullsplitter@die-optimisten.net>
2014-06-14 12:02:23 +0200

    parsing udp packets work

--- src/core/ipv4/broipinput.c
+++ src/core/ipv4/broipinput.c
@@ -1,5 +1,5 @@
 /*
- * Lwip: ipinput.c
+ * Lwip: broipinput.c
  * Part of BRO Project, 2014 <<https://github.com/BRO-FHV>>
  *
  * Created on: 13.06.2014
@@ -7,29 +7,66 @@
  * TODO
  */
 
-#include "lwip/broipinput.h"
-
-#include "lwip/opt.h"
-#include "lwip/ip.h"
-#include "lwip/def.h"
-#include "lwip/mem.h"
-#include "lwip/ip_frag.h"
-#include "lwip/inet_chksum.h"
-#include "lwip/netif.h"
-#include "lwip/icmp.h"
-#include "lwip/igmp.h"
-#include "lwip/raw.h"
-#include "lwip/udp.h"
-#include "lwip/tcp_impl.h"
-#include "lwip/snmp.h"
-#include "lwip/dhcp.h"
-#include "lwip/autoip.h"
-#include "lwip/stats.h"
-#include "arch/perf.h"
-
+#include <Syscall/lib_syscall.h>
+#include <inttypes.h>
 #include <string.h>
+#include "lwip/broipinput.h"
+#include "lwip/netif.h"
+
+#define MAX_DATA 1000
+
+uint8_t data[MAX_DATA];
+
+uint32_t isIPType(eth_header* header);
+uint16_t convertBigToLittleEndian(uint8_t data[]);
+uint32_t ipToInt(uint8_t data[]);
+uint32_t isPacketForUs(ip_header* header, struct netif *inp);
 
 err_t bro_ip_input(struct pbuf *p, struct netif *inp) {
+	eth_header* ethHeader = (eth_header*)p->payload;
+
+	if(isIPType(ethHeader)) {
+		ip_header* ipHeader = (ip_header*)ethHeader;
+
+		//check if packet is for us and if packet is a UDP packet
+		if(isPacketForUs(ipHeader, inp) && UDP_PROT == ipHeader->protocol) {
+			udp_header* udpHeader = (udp_header*)ipHeader;
+
+			//clear data - set everything to zero
+			memset(data, 0, MAX_DATA);
+
+			//data 1 and data 2 are not aligned
+			data[0] = udpHeader->data1;
+			data[1] = udpHeader->data2;
+
+			//copy rest of data aligned
+			//-10 because udpHeader->len is data header + data1 + data2 + dataRest (total packet length)
+			//we are just interesed in data
+			uint32_t dataLen = convertBigToLittleEndian(udpHeader->len) - 10;
+			memcpy(&data[2], &udpHeader->dataRestStart, dataLen);
+
+			bro_udp_input(ethHeader, ipHeader, udpHeader, data, dataLen);
+		}
+	}
 
 	return ERR_OK;
+}
+
+uint32_t isIPType(eth_header* header) {
+	return header->type[0] == 0x08 && header->type[1] == 0x00;
+}
+
+uint16_t convertBigToLittleEndian(uint8_t data[]) {
+	return data[1] + (data[0] << 8);
+}
+
+
+uint32_t ipToInt(uint8_t data[]) {
+	return data[0] + (data[1] << 8) + (data[2] << 16) + (data[3] << 24);
+}
+
+uint32_t isPacketForUs(ip_header* header, struct netif *inp) {
+	uint32_t dest = ipToInt(header->destIp);
+
+	return BROADCAST_ADDR == dest || ANY_ADDR == dest || ((ip_addr_t)inp->ip_addr).addr == dest;
 }
--- src/include/ipv4/lwip/broipinput.h
+++ src/include/ipv4/lwip/broipinput.h
@@ -10,23 +10,59 @@
 #ifndef IPINPUT_H_
 #define IPINPUT_H_
 
-#include "lwip/opt.h"
-#include "lwip/ip.h"
-#include "lwip/def.h"
-#include "lwip/mem.h"
-#include "lwip/ip_frag.h"
-#include "lwip/inet_chksum.h"
 #include "lwip/netif.h"
-#include "lwip/icmp.h"
-#include "lwip/igmp.h"
-#include "lwip/raw.h"
-#include "lwip/udp.h"
-#include "lwip/tcp_impl.h"
-#include "lwip/snmp.h"
-#include "lwip/dhcp.h"
-#include "lwip/autoip.h"
-#include "lwip/stats.h"
-#include "arch/perf.h"
+
+#define IP_ADDR_LENGTH		4
+#define MAC_ADDR_LENGTH 	6
+#define TYPE_LENGTH 		2
+#define IP_V4_TYPE			0x0800
+#define ARP_TYPE			0x0806
+#define UDP_PROT			17
+#define TCP_PROT			6
+#define BROADCAST_ADDR		4294967295
+#define ANY_ADDR			0
+
+
+typedef struct {
+	uint8_t destMacAddr[MAC_ADDR_LENGTH];
+	uint8_t srcMacAddr[MAC_ADDR_LENGTH];
+	uint8_t type[TYPE_LENGTH];
+} eth_header;
+
+
+typedef struct {
+	uint8_t ethHeader[14];
+
+	unsigned version:4;
+	unsigned ihl:4;
+	uint8_t tos;
+	uint16_t tot_len;
+	uint16_t ident;
+	uint16_t flags_offset;
+	uint8_t ttl;
+	uint8_t protocol;
+	uint16_t checksum;
+	uint8_t srcIp[IP_ADDR_LENGTH];
+	uint8_t destIp[IP_ADDR_LENGTH];
+} ip_header;
+
+typedef struct {
+	uint8_t ethHeader[14];
+	uint8_t ipHeader[20];
+
+	uint8_t srcPort[2];
+	uint8_t destPort[2];
+	uint8_t len[2];
+	uint8_t checksum[2];
+
+	uint8_t data1;
+	uint8_t data2;
+
+	uint8_t dataRestStart;
+} udp_header;
+
+
+extern void bro_udp_input(eth_header* ethHeader, ip_header* ipHeader, udp_header* udp_header, uint8_t* data, uint32_t dataLen);
 
 err_t bro_ip_input(struct pbuf *p, struct netif *inp);
 
